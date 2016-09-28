fe8a3c44b11f67e8fc6fd03a1092a73eea908bf6
jmunarb <munar.bestard.joan@gmail.com>
2016-09-28 20:32:13 +0200

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,57 @@
+
+
+public class Data {
+
+    //#1
+    private static final String SEPARADOR = "-";
+    private String data;
+
+    int dia;
+    int mes;
+    int any;
+
+        public String getData() {
+            return dia + SEPARADOR + mes + SEPARADOR + any;
+        }
+
+        public void setData(String data) throws Exception {
+            //#2
+            if (!data.contains(SEPARADOR)) throw new Exception();
+            int[] dates = descomposaData();
+
+            this.dia = dates[0];
+            this.mes = dates[1];
+            this.any = dates[2];
+        }
+
+        public int getAny(){
+            return this.any;
+        }
+
+        private int[] descomposaData(){
+
+            String[] dataDescomposada = data.split(SEPARADOR);
+            int[] dataDescomposadaInt = new int[dataDescomposada.length];
+            dataDescomposadaInt[0] = Integer.parseInt(dataDescomposada[0]);
+            dataDescomposadaInt[1] = Integer.parseInt(dataDescomposada[1]);
+            dataDescomposadaInt[2] = Integer.parseInt(dataDescomposada[2]);
+
+            return dataDescomposadaInt;
+
+        }
+
+    }
+
+class AnyDeTrespas extends Data{
+
+    private boolean anyPar(){
+
+        if(getAny() % 2 == 0){
+
+            return true;
+
+        }
+        return false;
+    }
+
+}
